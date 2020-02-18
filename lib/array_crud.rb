ad519f5d4caf076aa3b7cc9cb8e2f38eea47b0cb
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-02-17 17:24:56 -0800

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,46 +3,47 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Gabriel" ,"Bibi" ,"Ozzie" ,"Nenem"]
-  end
+  my_array = ["Gabriel", "Bibi", "Ozzie", "Bolinha"]
+end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Gabriel" ,"Bibi"]
-  array.push("arrays!")
+  my_array = ["Gabriel", "Bibi"]
+  my_array.push("arrays!")
+  p my_array
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["Gabriel" ,"Bibi"]
-  array.unshift("wow")
+  my_array = ["Gabriel", "Bibi", "Ozzie"]
+  my_array.unshift("wow")
+  p my_array
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["Gabriel" ,"Bibi" ,"arrays!"]
-  array.pop
+  my_array = ["Gabriel", "Bibi", "arrays!"]
+  my_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["wow" ,"Gabriel" ,"Bibi"]
-  array.shift 
+  my_array = ["wow", "Bibi", "arrays!"]
+  my_array.shift
 end
 
-
-def retrieve_element_from_index(array, index_number)
-  array = ["wow" ,"am" ,"Bibi"]
-  array[1]
+def retrieve_element_from_index(array, number)
+  my_array = ["am", "Bibi", "arrays!"]
+  my_array[0]
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["wow" ,"am" ,"Bibi"]
-  array[0]
+  my_array = ["wow", "Bibi", "arrays!"]
+  my_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["wow" ,"am" ,"arrays!"]
-  array[2]
+  my_array = ["wow", "Bibi", "arrays!"]
+  my_array[2]
 end
 
-def update_element_from_index(array, index_number, element)
-array = ["wow" ,"am" ,"arrays!"]
-array[-1] = "totally"
+def update_element_from_index(array, number, element)
+  my_array = ["wow", "Bibi", "arrays!", "Pai", "mae"]
+  my_array[4] = "totally"
 end
